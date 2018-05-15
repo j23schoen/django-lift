4dc89a67ac62a805925a62fc0cd0175ca41ab965
j23schoen <j23schoen@gmail.com>
2018-05-15 16:16:17 -0400

    created models and index method

---
+++ lift/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LiftConfig(AppConfig):
+    name = 'lift'
---
+++ lift/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class WorkoutSession(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date_of_workout = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField()
+
+class LiftEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    LIFTS = (
+        ('BP', 'Bench Press'),
+        ('SQ', 'Squat'),
+        ('OHP', 'Overhead Press'),
+        ('DL', 'Deadlift')
+    )
+
+    RPE = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+        ('7', '7'),
+        ('7.5', '7.5'),
+        ('8', '8'),
+        ('8.5', '8.5'),
+        ('9', '9'),
+        ('9.5', '9.5'),
+        ('10', '10'),
+    )
+
+    lift_type = models.CharField(max_length=3, choices=LIFTS)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    weight = models.IntegerField()
+    rpe = models.CharField(max_length=3, choices=RPE)
+    notes = models.TextField()
+
+    workout_session = models.ForeignKey(WorkoutSession, on_delete=models.CASCADE)
